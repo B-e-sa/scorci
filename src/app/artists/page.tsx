5142333fc001c0a58a75f1147dd20159c5b6b079
b_e_sa <joaospereira963@gmail.com>
2024-06-30 13:26:15 -0300

    finish artists page initial style

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -5,18 +5,20 @@ import Button from "../components/button";
 import artistsByCountry from "../data/byCountry";
 import style from "./style.module.scss";
 import { useDraggable } from "react-use-draggable-scroll";
+import { pxGrotesk } from "../fonts";
 
 export default function Artists() {
     const ref = useRef<HTMLDivElement>() as MutableRefObject<HTMLInputElement>;
     const { events } = useDraggable(ref);
 
     return (
-        <div className={`${style["page-container"]}`} style={{ userSelect: "none" }}>
-            <div style={{ borderRight: "2px solid #272727" }}>
+        <div className={`${pxGrotesk.className} ${style["page-container"]}`} style={{ userSelect: "none" }}>
+            <div className={`${style["button-container-wrapper"]}`}>
                 <div className={`${style["button-container"]}`}>
-                    <Button>LIST</Button>
                     <Button>FILTER</Button>
+                    <Button>LIST</Button>
                 </div>
+                <div className={`${style["bottom-box"]}`}></div>
             </div>
             <div
                 className={`${style["artists-container"]}`}
