571eab28e586eb5312dd56a035e78e0644ce9609
b_e_sa <joaospereira963@gmail.com>
2024-07-03 21:09:06 -0300

    add: artist destructuring

--- src/app/artists/[artist]/page.tsx
+++ src/app/artists/[artist]/page.tsx
@@ -5,18 +5,25 @@ import style from "./style.module.scss"
 import { useEffect, useState } from "react";
 import data from "@/app/data"
 import type { Artist } from "@/app/data";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { arno, pxGrotesk } from "@/app/fonts";
 import Button from "@/app/components/button/button";
+import Link from "next/link";
 
 export default function Artist() {
     const pathname = usePathname();
-    const [foundArtist, setFoundArtist] = useState<Artist>();
+    const [{
+        artworks,
+        borIn,
+        description,
+        diedIn,
+        fullName,
+        nickname,
+        portrait
+    }, setFoundArtist] = useState<Artist>({});
 
     const formatDateInfo = (date: string) => {
-        if (!date) {
-            return
-        }
+        if (!date) return;
 
         const dateArray = date.split("/");
         return `${dateArray[dateArray.length - 1]}, ${dateArray[dateArray.length - 2]}`
@@ -37,7 +44,7 @@ export default function Artist() {
         const foundArtist = data.language.pt.find((artist) => (
             artist.nickname === unslugify(regularNickname)
         ))
-
+        
         if (!foundArtist) return
 
         setFoundArtist(foundArtist);
@@ -46,55 +53,73 @@ export default function Artist() {
     return (
         <div className={`${style["info-grid"]} ${pxGrotesk.className}`}>
             <div className={`${style["first"]}`}>
-                <Button>BACK</Button>
+                <Link href={"/artists"}>
+                    <Button>BACK</Button>
+                </Link>
                 <div style={{ marginTop: 60 }}>
                     {
-                        foundArtist?.portrait &&
+                        portrait &&
                         <Image style={{ borderRadius: 999, objectFit: "cover" }}
                             alt=""
-                            src={foundArtist?.portrait}
+                            src={portrait}
                             height={80}
                             width={80}
                             unoptimized
                         />
                     }
                     <div className={`${style["name"]}`}>
-                        <p>{foundArtist?.nickname},</p>
-                        <p>{foundArtist?.fullName}</p>
+                        <p>{nickname},</p>
+                        <p title={fullName}>{fullName && fullName?.split(" ").length < 7 ? fullName : fullName?.split(" ").slice(0, 7).join(" ") + "..."}</p>
                     </div>
                     <p className={`color-secondary`}>Fatos rápidos</p>
                 </div>
             </div>
             <div className={`${style["quick-facts"]}`}>
                 <p className={`${arno.className}`}>
-                    Nascimento:<span>{formatDateInfo(foundArtist?.wasBorIn!)}</span>
+                    Nascimento:<span>{formatDateInfo(borIn!)}</span>
                 </p>
                 <p className={`${arno.className}`}>
-                    Falecimento: <span>{formatDateInfo(foundArtist?.diedIn!)}</span>
+                    Falecimento: <span>{formatDateInfo(diedIn!)}</span>
                 </p>
                 <p className={`${arno.className}`}>
-                    Most famous painting: <span></span>
-                </p>
-                <p className={`${arno.className}`}>
-                    "First" painting: <span></span>
-                </p>
-                <p className={`${arno.className}`}>
-                    "Last" painting: <span></span>
+                    Most famous painting: <span>{"unknown"}</span>
                 </p>
             </div>
             <div className={`${style["middle-column"]}`}>
                 {
-                    /*
-                    foundArtist?.artworks.map(() => {
+                    artworks && artworks!.map(({ image, completedIn, name }) => {
+                        // TODO: 
+                        // after switching all artworks urls for their images,
+                        // remove the instance checking
+                        if (image instanceof String) return
 
+                        return (
+                            <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }} key={name}>
+                                <div>
+                                    <p>{name}</p>
+                                    <p>{completedIn}</p>
+                                </div>
+                                <Image
+                                    alt=""
+                                    src={(image as StaticImageData).src}
+                                    width={500}
+                                    height={500}
+                                    style={{
+                                        width: "100%",
+                                        height: "45%",
+                                        minHeight: "45%",
+                                        objectFit: "cover"
+                                    }}
+                                />
+                            </div>
+                        )
                     })
-                    */
                 }
             </div>
             <div className={`${style["synopsis"]}`}>
                 <span>Sinopse</span>
                 <div>
-                    <p>{foundArtist?.description}</p>
+                    <p>{description}</p>
                 </div>
             </div>
             <div className={`${style["early-life"]}`}>
