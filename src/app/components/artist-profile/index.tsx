9358080a881f29068fc3fe83bfb18dd3352c62f4
b_e_sa <joaospereira963@gmail.com>
2024-07-10 15:44:02 -0300

    add div props

--- src/app/components/artist-profile/index.tsx
+++ src/app/components/artist-profile/index.tsx
@@ -3,12 +3,18 @@ import Image from "next/image";
 import style from "./style.module.scss";
 import { ComponentProps } from "react";
 
-type Props = ComponentProps<"div"> & Artist;
+type Props = ComponentProps<"div"> &
+  Artist & {
+    showFullname?: boolean;
+    infoStyle?: string;
+  };
 
 export default function ArtistProfile({
   portrait,
   nickname,
   fullName,
+  showFullname = true,
+  infoStyle,
   ...props
 }: Props) {
   return (
@@ -18,18 +24,20 @@ export default function ArtistProfile({
           style={{ borderRadius: 999, objectFit: "cover" }}
           alt=""
           src={portrait}
-          height={80}
-          width={80}
+          height={70}
+          width={70}
           unoptimized
         />
       )}
-      <div className={`${style["artist-profile"]}`}>
-        <p>{nickname},</p>
-        <p title={fullName}>
-          {fullName && fullName?.split(" ").length < 7
-            ? fullName
-            : fullName?.split(" ").slice(0, 7).join(" ") + "..."}
-        </p>
+      <div className={`${style["artist-profile"]} ${infoStyle}`}>
+        <p>{nickname}{showFullname && ","}</p>
+        {showFullname && (
+          <p title={fullName}>
+            {fullName && fullName?.split(" ").length < 7
+              ? fullName
+              : fullName?.split(" ").slice(0, 7).join(" ") + "..."}
+          </p>
+        )}
       </div>
     </div>
   );
