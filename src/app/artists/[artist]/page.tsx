35a1f619e9efb5b1e241604268c22202844c1e1f
b_e_sa <joaospereira963@gmail.com>
2024-07-10 15:43:19 -0300

    implements artwork scroller

--- src/app/artists/[artist]/page.tsx
+++ src/app/artists/[artist]/page.tsx
@@ -12,6 +12,8 @@ import { useEffect, useState } from "react";
 import unslugify from "../../utils/unslugify";
 import style from "./style.module.scss";
 import ArtistProfile from "@/app/components/artist-profile";
+import useMediaQuery from "@/app/hooks/useMediaQuery";
+import ArtoworkScroller from "@/app/components/artwork-scroller";
 
 export default function Artist() {
   const pathname = usePathname();
@@ -19,6 +21,7 @@ export default function Artist() {
     { artworks, borIn, description, diedIn, fullName, nickname, portrait },
     setFoundArtist,
   ] = useState<Artist>({});
+  const isSmallScreen = useMediaQuery(769);
 
   const formatDateInfo = (date: string) => {
     if (!date) return;
@@ -45,70 +48,30 @@ export default function Artist() {
         <Link href={"/artists"}>
           <Button>BACK</Button>
         </Link>
-        <div style={{ marginTop: 60 }}>
-          <ArtistProfile
-            portrait={portrait}
-            nickname={nickname}
-            fullName={fullName}
-          />
+        <div className={`${style["info-container"]}`}>
+          <ArtistProfile className={`${style["artist"]}`} infoStyle={`${style["info"]}`} {...{ portrait, nickname, fullName }} />
           <p className={`color-secondary`}>Fatos rápidos</p>
         </div>
       </div>
       <div className={`${style["quick-facts"]}`}>
         <p className={`${arno.className}`}>
-          Nascimento:<span>{formatDateInfo(borIn!)}</span>
+          Nascimento<span>{formatDateInfo(borIn!)}</span>
         </p>
         <p className={`${arno.className}`}>
-          Falecimento: <span>{formatDateInfo(diedIn!)}</span>
+          Falecimento <span>{formatDateInfo(diedIn!)}</span>
         </p>
         <p className={`${arno.className}`}>
-          Most famous painting: <span>{"unknown"}</span>
+          Most famous painting <span>{"unknown"}</span>
         </p>
       </div>
       <div className={`${style["middle-column"]}`}>
-        {artworks &&
-          artworks!.map(({ image, completedIn, name }) => {
-            // TODO:
-            // after switching all artworks urls for their images,
-            // remove the instance checking
-            if (image instanceof String) return;
-
-            return (
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                }}
-                key={name}
-              >
-                <div>
-                  <p>{name}</p>
-                  <p>{completedIn}</p>
-                </div>
-                <Link
-                  href={
-                    name
-                      ? `/artists/${pathname.split("/")[2]}/${slugify(name)}`
-                      : unslugify(pathname.split("/")[2])
-                  }
-                  style={{ width: "100%", height: "45%" }}
-                >
-                  <Image
-                    alt=""
-                    src={(image as StaticImageData).src}
-                    width={500}
-                    height={500}
-                    style={{
-                      width: "100%",
-                      height: "300px",
-                      objectFit: "cover",
-                    }}
-                  />
-                </Link>
-              </div>
-            );
-          })}
+        {artworks && (
+          <ArtoworkScroller
+            className={`${style["one-hundred-width"]}`}
+            artworks={artworks}
+            pathname={pathname}
+          />
+        )}
       </div>
       <div className={`${style["synopsis"]}`}>
         <span>Sinopse</span>
